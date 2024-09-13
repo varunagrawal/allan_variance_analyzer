94f3e0a97ffb9ff2c295b56f65f1526a18a703e7
Varun Agrawal <varagrawal@gmail.com>
2024-09-13 12:41:21 -0400

    Allan deviation analysis functions

---
+++ allan_variance/analysis.py
@@ -0,0 +1,341 @@
+"""
+Functions to perform analysis of the Allan Deviations to get the IMU parameters.
+"""
+
+from typing import Callable, List
+
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy.optimize import curve_fit
+
+
+def line_func(x, m, b):
+    """The line function y = mx + b"""
+    return m * x + b
+
+
+def get_intercept(x, y, m, b):
+    """Get the x and y intercepts of the line"""
+    logx = np.log(x)
+    logy = np.log(y)
+    # pylint: disable=unbalanced-tuple-unpacking
+    coeffs, _ = curve_fit(line_func,
+                          logx,
+                          logy,
+                          bounds=([m, -np.inf], [m + 0.001, np.inf]))
+    poly = np.poly1d(coeffs)
+
+    def yfit(x):
+        return np.exp(poly(np.log(x)))
+
+    return yfit(b), yfit
+
+
+def generate_prediction(tau,
+                        q_quantization=0,
+                        q_white=0,
+                        q_bias_instability=0,
+                        q_walk=0,
+                        q_ramp=0):
+    """
+    Given fitted IMU noise model, generate measurement prediction at `tau`.
+    """
+    n = len(tau)
+
+    A = np.empty((n, 5))
+    A[:, 0] = 3 / tau**2
+    A[:, 1] = 1 / tau
+    A[:, 2] = 2 * np.log(2) / np.pi
+    A[:, 3] = tau / 3
+    A[:, 4] = tau**2 / 2
+
+    params = np.array([
+        q_quantization**2, q_white**2, q_bias_instability**2, q_walk**2,
+        q_ramp**2
+    ])
+
+    return np.sqrt(A.dot(params))
+
+
+def compute_white_noise_params(period, measurements, white_noise_break_point):
+    """
+    Compute the ARW/VRW generated from the white noise component.
+    Found by fitting a line with gradient=-0.5 at intercept t=1
+    """
+    wn_intercept = np.zeros(3)
+    wn_fit_fn = [None] * 3
+    for idx, _ in enumerate("xyz"):
+        wn_intercept[idx], wn_fit_fn[idx] = get_intercept(
+            period[0:white_noise_break_point],
+            measurements[0:white_noise_break_point, idx], -0.5, 1.0)
+
+    return wn_intercept, wn_fit_fn
+
+
+def compute_rate_random_walk(period, measurements):
+    """
+    Compute the Rate Random Walk for the given measurements.
+    This corresponds to the y-intercept of the line with gradient=0.5 and x-intercept=3.0.
+    """
+    rr_intercept = np.zeros(3)
+    rr_fit_fn = [None] * 3
+    for idx, _ in enumerate("xyz"):
+        axis_measurements = measurements[:, idx]
+        rr_intercept[idx], rr_fit_fn[idx] = get_intercept(
+            period, axis_measurements, 0.5, 3.0)
+
+    return rr_intercept, rr_fit_fn
+
+
+def compute_bias_instability(measurement: np.ndarray):
+    """Compute the bias instability values."""
+    measurement_min = np.amin(measurement, axis=0)
+    measurement_argmin = np.argmin(measurement, axis=0)
+    return measurement_min, measurement_argmin
+
+
+def plot_loglog(period: np.ndarray,
+                measurements: np.ndarray,
+                fit_wn: List[Callable],
+                fit_rr: List[Callable],
+                wn_intercept: List[float],
+                rr_intercept: List[float],
+                measurement_min: List[float],
+                measurement_min_index: List[int],
+                average_white_noise: float,
+                average_bias_instability: float,
+                average_random_walk: float,
+                measurement_type: str,
+                sensor_type: str,
+                units: str,
+                dpi=90,
+                figsize=(16, 9)):
+    """Plot Allan Deviations on LogLog scale.
+
+    Args:
+        period (np.ndarray): _description_
+        measurements (np.ndarray): _description_
+        fit_wn (List[Callable]): _description_
+        fit_rr (List[Callable]): _description_
+        wn_intercept (List[float]): _description_
+        rr_intercept (List[float]): _description_
+        measurement_min (List[float]): _description_
+        measurement_min_index (List[int]): _description_
+        average_white_noise (float): _description_
+        average_bias_instability (float): _description_
+        average_random_walk (float): _description_
+        measurement_type (str): _description_
+        sensor_type (str): _description_
+        units (str): _description_
+        dpi (int, optional): _description_. Defaults to 90.
+        figsize (tuple, optional): _description_. Defaults to (16, 9).
+    """
+
+    fig = plt.figure(num=measurement_type, dpi=dpi, figsize=figsize)
+
+    plt.loglog(period, measurements[:, 0], "r--", label='X')
+    plt.loglog(period, measurements[:, 1], "g--", label='Y')
+    plt.loglog(period, measurements[:, 2], "b--", label='Z')
+
+    for idx, c in enumerate("rgb"):
+        if idx == 2:
+            wn_label = "White noise fit line"
+            rr_label = "Random Rate fit line"
+        else:
+            wn_label = ""
+            rr_label = ""
+
+        plt.loglog(period, fit_wn[idx](period), "m-", label=wn_label)
+        plt.loglog(period, fit_rr[idx](period), "y-", label=rr_label)
+
+        plt.loglog(1.0, wn_intercept[idx], f"{c}o", markersize=20)
+        plt.loglog(3.0, rr_intercept[idx], f"{c}*", markersize=20)
+
+        plt.loglog(period[measurement_min_index[idx]],
+                   measurement_min[idx],
+                   f"{c}^",
+                   markersize=20)
+
+    fitted_model = generate_prediction(
+        period,
+        q_white=average_white_noise,
+        q_bias_instability=average_bias_instability,
+        q_walk=average_random_walk)
+    plt.loglog(period, fitted_model, "-k", label='fitted model')
+
+    plt.title(sensor_type, fontsize=30)
+    plt.ylabel(f"Allan Deviation {units}", fontsize=30)
+    plt.legend(fontsize=25)
+    plt.grid(True)
+    plt.xlabel("Period (s)", fontsize=30)
+    plt.tight_layout()
+
+    plt.draw()
+    # plt.pause(1)
+    # w = plt.waitforbuttonpress(timeout=5)
+    plt.close()
+
+    fig.savefig(f'{measurement_type.lower()}.png',
+                dpi=600,
+                bbox_inches="tight")
+
+
+def accelerometer_analysis(period, acceleration, white_noise_break_point):
+    """Analyze the accelerometer measurements to get accelerometer parameters."""
+    # Compute VRW from the white noise
+    # gradient=-0.5, intercept at t=1
+    accel_wn_intercept, accel_fit_wn = compute_white_noise_params(
+        period, acceleration, white_noise_break_point)
+
+    # Compute rate random walk
+    # gradient=0.5, intercept at t=3
+    accel_rr_intercept, accel_fit_rr = compute_rate_random_walk(
+        period, acceleration)
+
+    accel_min, accel_min_index = compute_bias_instability(acceleration)
+
+    print("ACCELEROMETER:")
+    for idx, axis in enumerate("XYZ"):
+        print(
+            f"{axis} Velocity Random Walk: {accel_wn_intercept[idx]: .5f} m/s/sqrt(s)",
+            f"{accel_wn_intercept[idx]*60: .5f} m/s/sqrt(hr)")
+
+    for idx, axis in enumerate("XYZ"):
+        print(f"{axis} Bias Instability: {accel_min[idx]: .5f} m/s^2",
+              f"{accel_min[idx]*3600*3600: .5f} m/hr^2")
+
+    for idx, axis in enumerate("XYZ"):
+        print(
+            f"{axis} Accel Random Walk: {accel_rr_intercept[idx]: .5f} m/s^2/sqrt(s)"
+        )
+
+    average_acc_white_noise = accel_wn_intercept.mean()
+    average_acc_bias_instability = accel_min.mean()
+    average_acc_random_walk = accel_rr_intercept.mean()
+
+    # use worst value
+    worst_accel_white_noise = np.amax(accel_wn_intercept)
+    worst_accel_random_walk = np.amax(accel_rr_intercept)
+
+    plot_loglog(period, acceleration, accel_fit_wn, accel_fit_rr,
+                accel_wn_intercept, accel_rr_intercept, accel_min,
+                accel_min_index, average_acc_white_noise,
+                average_acc_bias_instability, average_acc_random_walk,
+                "Acceleration", "Accelerometer", "m/s^2")
+
+    return worst_accel_white_noise, worst_accel_random_walk
+
+
+def gyroscope_analysis(period, rotation_rate, white_noise_break_point):
+    """Analyze the gyroscope measurements to get gyroscope parameters."""
+    # Compute ARW from the white noise
+    # gradient=-0.5, intercept at t=1
+    gyro_wn_intercept, gyro_fit_wn = compute_white_noise_params(
+        period, rotation_rate, white_noise_break_point)
+
+    # Compute rate random walk
+    # gradient=0.5, intercept at t=3
+    gyro_rr_intercept, gyro_fit_rr = compute_rate_random_walk(
+        period, rotation_rate)
+
+    gyro_min, gyro_min_index = compute_bias_instability(rotation_rate)
+
+    print("GYROSCOPE:")
+    for idx, axis in enumerate("XYZ"):
+        print(
+            f"{axis} Angle Random Walk: {gyro_wn_intercept[idx]: .5f} deg/sqrt(s)",
+            f"{gyro_wn_intercept[idx] * 60: .5f} deg/sqrt(hr)")
+
+    for idx, axis in enumerate("XYZ"):
+        print(
+            f"{axis} Bias Instability: {gyro_min[idx]: .5f} deg/s {gyro_min[idx]*60*60: .5f} deg/hr"
+        )
+
+    for idx, axis in enumerate("XYZ"):
+        print(
+            f"{axis} Rate Random Walk: {gyro_rr_intercept[idx]: .5f} deg/s/sqrt(s)"
+        )
+
+    average_gyro_white_noise = gyro_wn_intercept.mean()
+    average_gyro_bias_instability = gyro_min.mean()
+    average_gyro_random_walk = gyro_rr_intercept.mean()
+
+    # use worst value
+    worst_gyro_white_noise = np.amax(gyro_wn_intercept)
+    worst_gyro_random_walk = np.amax(gyro_rr_intercept)
+
+    plot_loglog(period,
+                rotation_rate,
+                gyro_fit_wn,
+                gyro_fit_rr,
+                gyro_wn_intercept,
+                gyro_rr_intercept,
+                gyro_min,
+                gyro_min_index,
+                average_gyro_white_noise,
+                average_gyro_bias_instability,
+                average_gyro_random_walk,
+                measurement_type="Gyro",
+                sensor_type="Gyroscope",
+                units="deg/s")
+
+    return worst_gyro_white_noise, worst_gyro_random_walk
+
+
+def write_imu_yaml(worst_accel_white_noise: float,
+                   worst_accel_random_walk: float,
+                   worst_gyro_white_noise: float,
+                   worst_gyro_random_walk: float, update_rate: int):
+    """
+    Write IMU calibration parameters to YAML file.
+
+    Args:
+        worst_accel_white_noise (float): Accelerometer white noise
+        worst_accel_random_walk (float): Accelerometer bias random walk
+        worst_gyro_white_noise (float): Gyroscope white noise
+        worst_gyro_random_walk (float): Gyroscope bias random walk
+        update_rate (int): The IMU update rate.
+    """
+    print("Writing Kalibr imu.yaml file.")
+    with open("imu.yaml", "w") as yaml_file:
+
+        yaml_file.write("#Accelerometer\n")
+        yaml_file.write(
+            f"accelerometer_noise_density: {worst_accel_white_noise}\n")
+        yaml_file.write(
+            f"accelerometer_random_walk: {worst_accel_random_walk}\n")
+        yaml_file.write("\n")
+
+        yaml_file.write("#Gyroscope\n")
+        # Convert back to radians here
+        yaml_file.write(
+            f"gyroscope_noise_density: {worst_gyro_white_noise * np.pi / 180}\n"
+        )
+        yaml_file.write(
+            f"gyroscope_random_walk: {worst_gyro_random_walk * np.pi / 180}\n")
+        yaml_file.write("\n")
+
+        yaml_file.write(
+            f"update_rate: {update_rate} #Make sure this is correct\n")
+
+    print("Make sure to update rostopic and rate.")
+
+
+def analyze(period, allan_deviations, update_rate: int):
+    """Analyze the Allan Deviations to get IMU parameters"""
+    acceleration = allan_deviations[:, 0:3]
+    rotation_rate = allan_deviations[:, 3:6]
+
+    white_noise_break_point = np.where(period == 10)[0][0]
+
+    worst_accel_white_noise, worst_accel_random_walk = accelerometer_analysis(
+        period, acceleration, white_noise_break_point)
+
+    worst_gyro_white_noise, worst_gyro_random_walk = gyroscope_analysis(
+        period, rotation_rate, white_noise_break_point)
+
+    write_imu_yaml(worst_accel_white_noise,
+                   worst_accel_random_walk,
+                   worst_gyro_white_noise,
+                   worst_gyro_random_walk,
+                   update_rate=update_rate)
---
+++ tests/test_analysis.py
@@ -0,0 +1,126 @@
+"""Tests for analysis module"""
+
+import unittest
+from pathlib import Path
+from typing import Callable
+
+import numpy as np
+
+from allan_variance.analysis import (accelerometer_analysis,
+                                     compute_bias_instability,
+                                     compute_rate_random_walk,
+                                     compute_white_noise_params, get_intercept,
+                                     gyroscope_analysis)
+
+current_dir = Path(__file__).parent.absolute()
+
+
+class TestAnalysis(unittest.TestCase):
+    """Tests for functions which analyze the Allan Deviations."""
+
+    def setUp(self):
+        data = np.loadtxt(current_dir / "fixtures" / "allan_variance.csv",
+                          delimiter=' ',
+                          dtype=float,
+                          usecols=(0, 1, 2, 3, 4, 5, 6))
+
+        self.period = data[:, 0]
+        self.allan_deviations = data[:, 1:]
+
+        self.white_noise_break_point = np.where(self.period == 10)[0][0]
+
+        self.acceleration = self.allan_deviations[:, 0:3]
+        self.rotation_rate = self.allan_deviations[:, 3:6]
+
+    def test_compute_white_noise_params(self):
+        """Test the compute_white_noise_params function."""
+        accel_wn_intercept, accel_fit_wn = compute_white_noise_params(
+            self.period, self.acceleration, self.white_noise_break_point)
+
+        expected_accel_white_noise_params = np.asarray([
+            0.0025282402102088882, 0.0025615284313200383, 0.002466368970143399
+        ])
+
+        # regression
+        np.testing.assert_allclose(expected_accel_white_noise_params,
+                                   accel_wn_intercept)
+
+        self.assertIsInstance(accel_fit_wn, list)
+        for fn in accel_fit_wn:
+            self.assertIsInstance(fn, Callable)
+
+        gyro_wn_intercept, gyro_fit_wn = compute_white_noise_params(
+            self.period, self.rotation_rate, self.white_noise_break_point)
+
+        expected_gyro_white_noise_params = np.asarray(
+            [0.010742013458498411, 0.010936249927095876, 0.010784944419707091])
+
+        # regression
+        np.testing.assert_allclose(expected_gyro_white_noise_params,
+                                   gyro_wn_intercept)
+
+        self.assertIsInstance(gyro_fit_wn, list)
+        for fn in gyro_fit_wn:
+            self.assertIsInstance(fn, Callable)
+
+    def test_compute_rate_random_walk(self):
+        """Test compute_rate_random_walk"""
+        accel_rr_intercept_x, xfit_rr = get_intercept(self.period,
+                                                      self.acceleration[:, 0],
+                                                      0.5, 3.0)
+        accel_rr_intercept_y, yfit_rr = get_intercept(self.period,
+                                                      self.acceleration[:, 1],
+                                                      0.5, 3.0)
+        accel_rr_intercept_z, zfit_rr = get_intercept(self.period,
+                                                      self.acceleration[:, 2],
+                                                      0.5, 3.0)
+
+        accel_rr_intercept, fit_rr = compute_rate_random_walk(
+            self.period, self.acceleration)
+
+        np.testing.assert_allclose(
+            np.asarray([
+                accel_rr_intercept_x, accel_rr_intercept_y,
+                accel_rr_intercept_z
+            ]), accel_rr_intercept)
+
+        for idx, fit_fn in enumerate((xfit_rr, yfit_rr, zfit_rr)):
+            x = np.arange(1, 100, 0.1)
+            np.testing.assert_allclose(fit_fn(x), fit_rr[idx](x))
+
+    def test_compute_bias_instability(self):
+        """Test compute_bias_instability"""
+        accel_min_x = np.amin(self.acceleration[:, 0])
+        accel_min_y = np.amin(self.acceleration[:, 1])
+        accel_min_z = np.amin(self.acceleration[:, 2])
+
+        accel_min_x_index = np.argmin(self.acceleration[:, 0])
+        accel_min_y_index = np.argmin(self.acceleration[:, 1])
+        accel_min_z_index = np.argmin(self.acceleration[:, 2])
+
+        accel_min, accel_argmin = compute_bias_instability(self.acceleration)
+
+        np.testing.assert_allclose(
+            np.asarray([accel_min_x, accel_min_y, accel_min_z]), accel_min)
+        np.testing.assert_allclose(
+            np.asarray(
+                [accel_min_x_index, accel_min_y_index, accel_min_z_index]),
+            accel_argmin)
+
+    def test_accelerometer_analysis(self):
+        """Test accelerometer_analysis function."""
+        worst_accel_white_noise, worst_accel_random_walk = accelerometer_analysis(
+            self.period, self.acceleration, self.white_noise_break_point)
+
+        # regression
+        self.assertEqual(0.0025615284313200383, worst_accel_white_noise)
+        self.assertEqual(9.079971439106243e-05, worst_accel_random_walk)
+
+    def test_gyroscope_analysis(self):
+        """Test gyroscope_analysis function."""
+        worst_gyro_white_noise, worst_gyro_random_walk = gyroscope_analysis(
+            self.period, self.rotation_rate, self.white_noise_break_point)
+
+        # regression
+        self.assertEqual(0.010936249927095876, worst_gyro_white_noise)
+        self.assertEqual(0.0002359578060906847, worst_gyro_random_walk)
