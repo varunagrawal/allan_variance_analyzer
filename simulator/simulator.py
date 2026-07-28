c18ada5f83a7402d0a9032385e37b57eb9c25de0
Varun Agrawal <varagrawal@gmail.com>
2026-07-27 22:24:49 -0400

    Cleanup simulator

--- simulator/simulator.py
+++ simulator/simulator.py
@@ -2,7 +2,7 @@
 https://github.com/ori-drs/allan_variance_ros/blob/master/src/ImuSimulator.cpp
 """
 
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from pathlib import Path
 
 import jax
@@ -17,20 +17,22 @@ from allan_variance_analyzer.base import FilePath
 
 def RandomNormalDistributionVector(key: jax.Array, sigma: float):
     """Sample a normally distributed vector with variance `sigma`."""
-    return random.normal(key, shape=(3, )) * sigma
+    return random.normal(key, shape=(3,)) * sigma
 
 
 class ImuSimulator:
     """An IMU simulator for testing and debugging."""
 
     def __init__(self, config_file: FilePath, output_path: FilePath):
-        with open(config_file, 'r') as stream:
+        with open(config_file, "r") as stream:
             self.config_ = yaml.safe_load(stream)
 
         self.accelerometer_noise_density_: float = self.config_[
-            "accelerometer_noise_density"]
+            "accelerometer_noise_density"
+        ]
         self.accelerometer_random_walk_: float = self.config_[
-            "accelerometer_random_walk"]
+            "accelerometer_random_walk"
+        ]
         self.accelerometer_bias_init_ = self.config_["accelerometer_bias_init"]
 
         self.gyroscope_noise_density_ = self.config_["gyroscope_noise_density"]
@@ -52,34 +54,46 @@ class ImuSimulator:
         logger.info("Generating IMU data...")
         dt = 1 / self.update_rate_
 
-        accelerometer_bias = np.full((3, ), self.accelerometer_bias_init_)
-        gyroscope_bias = np.full((3, ), self.gyroscope_bias_init_)
+        accelerometer_bias = np.full((3,), self.accelerometer_bias_init_)
+        gyroscope_bias = np.full((3,), self.gyroscope_bias_init_)
         accelerometer_real = np.zeros(3)
         gyroscope_real = np.zeros(3)
 
         measurements_file = self.output_path_ / "measurements.csv"
-        writer = open(measurements_file, 'w+')
 
-        start_time = datetime.now()
+        with open(measurements_file, "w+") as writer:
+            start_time = datetime.now(tz=timezone.utc)
 
-        for i in tqdm(range(int(self.sequence_time_ * self.update_rate_))):
-            # Reference: https://github.com/ethz-asl/kalibr/wiki/IMU-Noise-Model
-            accelerometer_bias += RandomNormalDistributionVector(
-                key, self.accelerometer_random_walk_) * np.sqrt(dt)
-            gyroscope_bias += RandomNormalDistributionVector(
-                key, self.gyroscope_random_walk_) * np.sqrt(dt)
+            for i in tqdm(range(int(self.sequence_time_ * self.update_rate_))):
+                # Reference: https://github.com/ethz-asl/kalibr/wiki/IMU-Noise-Model
+                accelerometer_bias += RandomNormalDistributionVector(
+                    key, self.accelerometer_random_walk_
+                ) * np.sqrt(dt)
+                gyroscope_bias += RandomNormalDistributionVector(
+                    key, self.gyroscope_random_walk_
+                ) * np.sqrt(dt)
 
-            acc_measure = accelerometer_real + accelerometer_bias + RandomNormalDistributionVector(
-                key, self.accelerometer_noise_density_) / np.sqrt(dt)
-            gyro_measure = gyroscope_real + gyroscope_bias + RandomNormalDistributionVector(
-                key, self.gyroscope_noise_density_) / np.sqrt(dt)
+                acc_measure = (
+                    accelerometer_real
+                    + accelerometer_bias
+                    + RandomNormalDistributionVector(
+                        key, self.accelerometer_noise_density_
+                    )
+                    / np.sqrt(dt)
+                )
+                gyro_measure = (
+                    gyroscope_real
+                    + gyroscope_bias
+                    + RandomNormalDistributionVector(key, self.gyroscope_noise_density_)
+                    / np.sqrt(dt)
+                )
 
-            timestamp = start_time + timedelta(0, i / self.update_rate_)
-            ts = timestamp.strftime('%S.%f')
+                timestamp = start_time + timedelta(0, i / self.update_rate_)
+                ts = timestamp.strftime("%S.%f")
 
-            writer.write(
-                f"{i},{ts},{acc_measure[0]},{acc_measure[1]},{acc_measure[2]},{gyro_measure[0]},{gyro_measure[1]},{gyro_measure[2]}\n"
-            )
+                writer.write(
+                    f"{i},{ts},{acc_measure[0]},{acc_measure[1]},{acc_measure[2]},{gyro_measure[0]},{gyro_measure[1]},{gyro_measure[2]}\n"
+                )
 
         logger.info("Finished generating data. ")
         writer.close()
@@ -87,5 +101,5 @@ class ImuSimulator:
 
 
 if __name__ == "__main__":
-    simulator = ImuSimulator("config/imu_simulator.yaml", ".")
+    simulator = ImuSimulator("imu_simulator_config.yaml", ".")
     simulator.run()
